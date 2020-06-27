ed1064eb3ade25a73a8d8ca57dc8eeb499b69263
liuhaining <2388567009@qq.com>
2020-06-27 10:35:22 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './route/router';
+import echarts from 'echarts';
+import Antd from 'ant-design-vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import moment from 'moment';
+import Mock from 'mockjs'
+import * as filters from './api/filter.js'
+import 'ant-design-vue/dist/antd.css';
+import './assets/styles/common/index.less';
+import 'moment/locale/zh-cn'
+
+import store from '@/store/index'
+
+moment.locale('zh-cn');
+
+
+Vue.config.productionTip = false;
+Vue.prototype.$echarts = echarts;
+Vue.prototype.$moment = moment;
+Vue.prototype.$mock = Mock;
+Vue.use(VueAxios, axios);
+Vue.use(Antd);
+
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+router.beforeEach((to, from, next) => {
+  if (!store.state.UserToken) {
+    //do something => getUserInfo
+    next()
+  } else {
+      
+  }
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/route/router.js
@@ -0,0 +1,153 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../views/Home.vue'
+import loadable from './loadable'
+import mainView from '../page/trans-bank/mainView.vue'
+
+// const Foo = () => import(/* webpackChunkName: "group-foo" */ './Foo.vue')
+
+const ManageAnalysis = () => import('../page/manage-analysis')
+const Debt = () => import('../page/manage-analysis/debt.vue')
+const Assect = ()=> import('../page/manage-analysis/assets.vue')
+const Income = ()=> import('../page/manage-analysis/income.vue')
+
+// 交易银行 管理分析
+const Product = ()=> import('../page/manage-analysis/product.vue')
+const Hold = ()=> import('../page/manage-analysis/product/hold.vue')
+const Occur = ()=> import('../page/manage-analysis/product/occur.vue')
+const CusAnaly = ()=> import('../page/customer-analysis/index.vue')
+
+// 小企业 管理分析
+const Small = ()=> import('../page/small')
+const ManageHome = ()=> import('../page/small/manageHome.vue')
+const ManageDebts = ()=> import('../page/small/debts.vue')
+const ManageAssets = ()=> import('../page/small/assets.vue')
+const ManageOthers = ()=> import('../page/small/others.vue')
+const CustomView = ()=> import('../page/small/custome.vue')
+const ProductView = ()=> import('../page/small/product.vue')
+const interestRateView = ()=> import('../page/small/interestRate.vue')
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+      path: '/test',
+      name: 'home', 
+      component: Home
+    },
+    {
+      path: '/mainView',
+      component: mainView,
+    },
+    {
+      path: '/manage',
+      component: loadable(ManageAnalysis),
+      children: [
+        {
+          path: '/',
+          redirect:"debts"
+        },
+        {
+          path: 'debts',
+          component: loadable(Debt),
+        },
+        {
+          path: 'money',
+          component: loadable(Assect),
+        },
+        {
+          path: 'salary',
+          component: loadable(Income),
+        },
+        {
+          path: 'prd',
+          component: loadable(Product),
+          children:[
+            {
+              path: '/',
+              redirect:'hold'
+            },
+            {
+              path: 'hold',
+              component: loadable(Hold),
+            },
+            {
+              path: 'occur',
+              component: loadable(Occur),
+            },
+          ]
+        },
+      ]
+    },
+    {
+      path: '/analysis',
+      component: loadable(CusAnaly),
+    },
+    {
+      path: '/small',
+      component: loadable(Small),
+      children: [
+        {
+          path: '/manageSmall',
+          component: loadable(Small),
+          children: [
+            {
+              path: '/',
+              redirect: 'home'
+            },
+            {
+              path: 'home',
+              component: loadable(ManageHome),
+            },
+            {
+              path: 'debts',
+              component: loadable(ManageDebts),
+            },
+            {
+              path: 'assets',
+              component: loadable(ManageAssets),
+            },
+            {
+              path: 'others',
+              component: loadable(ManageOthers),
+            }
+
+          ]
+        },
+        {
+          path: '/smallView',
+          component: loadable(Small),
+          children: [
+            {
+              path: '/',
+              redirect: 'home'
+            },
+            {
+              path: 'custom',
+              component: loadable(CustomView),
+            },
+            {
+              path: 'product',
+              component: loadable(ProductView),
+            }
+
+          ]
+        },
+        {
+          path: '/smallAssess',
+          component: loadable(Small),
+          children: [
+            {
+              path: '/',
+              redirect: 'interestRate'
+            },
+            {
+              path: 'interestRate',
+              component: loadable(interestRateView),
+            },
+          ]
+        }
+      ]
+    }
+  ]
+})
