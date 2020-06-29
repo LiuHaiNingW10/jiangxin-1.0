40aa951e41544bfca35ea66d047f3059e28eec08
liuhaining <2388567009@qq.com>
2020-06-29 21:54:40 +0800

    p4 and assets.png

---
+++ src/components/p4/pop-custom.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="popToRight">
+    <div class="pop-title">
+      <span>{{id.title}}</span>
+    </div>
+    <div class="pop-content">
+      <div class="pop-item pop-a">
+          <span>未拨通</span><br>
+          <i>67<br>人</i>
+      </div>
+      <div class="pop-item pop-b">
+          <span>最近承诺还款</span><br>
+          <i>2637<br>人</i>
+      </div>
+      <div class="pop-item pop-c">
+          <span>未接听</span><br>
+          <i>12<br>人</i>
+      </div>
+      <div class="pop-item pop-d">
+          <span>延迟还款</span><br>
+          <i>67<br>人</i>
+      </div>
+      <div class="pop-item pop-e">
+          <span>通话中断</span><br>
+          <i style="color:#09C937">37<br>人</i>
+      </div>
+      <div class="pop-item pop-f">
+          <span>承诺马上还款</span><br>
+          <i>1237<br>人</i>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default {
+  name: "tableAuto",
+  props: ["tableDatas", "ids", "columns", "heights"],
+  data() {
+    return {
+      tableData: this.tableDatas,
+      id: this.ids,
+      height: this.heights,
+      lineData: []
+    };
+  },
+  computed: {},
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      console.log(this.tableData);
+      // this.transLateData();
+    },
+    transLateData() {
+      let obj = {
+        type: "bar",
+        yAxis: [],
+        series: []
+      };
+      this.tableData.forEach(el => {
+        obj.series.push(el.num);
+        obj.yAxis.push(el.categroy);
+      });
+      this.lineData = obj.series;
+      this.drawLineG(this.id.id, obj);
+    },
+    
+  }
+};
+</script>
+<style lang="less" scoped>
+
+.popToRight {
+  font-size: 12px;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 356px;
+  background: url("../../assets/images/p4-table-title.png") no-repeat center;
+  background-size: 100% 100%;
+  padding: 14px 10px;
+  .pop-title {
+    text-align: left;
+    padding-left: 24px;
+  }
+  .pop-content {
+    text-align: center;
+    padding: 30px 0;
+    .pop-item {
+      text-align: center;
+      display: inline-block;
+      i {
+        font-style: normal;
+        font-size: 14px;
+      }
+    }
+    .pop-a {
+      height: 140px;
+      width: 33%;
+      background: url('../../assets/images/circle/1-lv.png') no-repeat center;
+      background-size: 100%;
+      padding-top: 38px;
+      i {
+        color: #E19513;
+      } 
+      
+    }
+    .pop-b {
+      height: 120px;
+      width: 30%;
+      background: url('../../assets/images/circle/oval.png') no-repeat center;
+      background-size: 100%;
+      padding-top: 38px;
+      i {
+        color: #17D9F2;
+      }
+    }
+    .pop-c {
+      height: 120px;
+      width: 30%;
+      background: url('../../assets/images/circle/1-lv.png') no-repeat center;
+      background-size: 100%;
+      padding-top: 38px;
+      i {
+        color: #E19513;
+      }
+    }
+    .pop-d {
+      height: 140px;
+      width: 33%;
+      background: url('../../assets/images/circle/oval.png') no-repeat center;
+      background-size: 80%;
+      padding-top: 38px;
+      i {
+        color: #ABD043;
+      } 
+      
+    }
+    .pop-e {
+      height: 120px;
+      width: 30%;
+      background: url('../../assets/images/circle/1-lv.png') no-repeat center;
+      background-size: 100%;
+      padding-top: 38px;
+      i {
+        color: #17D9F2;
+      }
+    }
+    .pop-f {
+      height: 120px;
+      width: 30%;
+      background: url('../../assets/images/circle/oval.png') no-repeat center;
+      background-size: 95%;
+      padding-top: 38px;
+      i {
+        color: #17D9F2;
+      }
+    }
+  }
+  
+}
+
+</style>
--- src/page/small/p4.vue
+++ src/page/small/p4.vue
@@ -1,29 +1,42 @@
 <template>
 <div class="container">
-    <div class="header">百信智能聚变引擎</div>
+    <div :class="headValue ? 'brain-bg':'brain-bgg'">
+      <div class="header">
+      <span class="header-bg"></span>
+      <div class="right-time cf">
+          <div>
+            <img style="width:4%" :src="currentImg" alt />
+            <span class style="padding-right: 20px;">{{currentWeather.high}} ℃ ~ {{currentWeather.low}} ℃</span>
+            <span class="time-span">{{currentTime.date}} </span>
+            <span class="time-span" style="padding-right: 20px;">{{currentTime.time}}</span>
+          </div>
+      </div>
+    </div>
     <div class="main-content">
       <div class="left">
           <table-auto :tableDatas='tableDataA' :ids='idA' :columns="columnA" :heights="clientHeight" />
           <line-right :tableDatas='tableDataB' :ids='idB' :heights="clientHeight" />
       </div>
       <div class="center">
-        <big-head :tableDatas='tableDataC' :ids='idC' :heights="clientHeight" />
+        <big-head :tableDatas='tableDataC' :ids='idC' :heights="clientHeight" @func="fromHead"/>
       </div>
       <div class="right">
           <table-auto :tableDatas='tableDataC' :ids='idC' :columns="columnC" :heights="clientHeight" />
-          <div style="width: 100%;height:318px">
-
-          </div>
+          <pop-custom :tableDatas='tableDataD' :ids='idD' :heights="clientHeight" />
       </div>
+    </div>
     </div>
 </div>
 </template>
 
 <script>
 import Vue from 'vue'
+import Yin from "../../assets/images/yin.png";
+import Yu from "../../assets/images/yu.png";
 import tableAuto from '../../components/p4/autoScroll-table.vue'
 import lineToRight from '../../components/p4/line-toRight.vue'
 import bigHeadVue from '../../components/p4/big-head.vue'
+import popCustom from '../../components/p4/pop-custom.vue'
 
 const columnA = [
   {
@@ -97,6 +110,24 @@ const columnC = [
 export default {
   data() {
     return {
+      headValue: true,
+      // 时间
+      currentTime: {},
+      currentDate: undefined,
+      weatherImg: [Yin, Yu, Yu, Yu, Yu, Yu, Yu],
+      currentWeather: {},
+      currentImg: "",
+      // 天气
+      localweather: [
+        { high: "29", low: "22" },
+        { high: "28", low: "23" },
+        { high: "27", low: "23" },
+        { high: "25", low: "23" },
+        { high: "26", low: "22" },
+
+        { high: "29", low: "22" },
+        { high: "29", low: "22" }
+      ],
       tableDataA:[
         {
           telphoneNum: 188 + '****'+ (Math.random() * 9000 + 1000).toFixed(0),
@@ -305,20 +336,82 @@ export default {
         title: '智能客服'
       },
       columnC: columnC,
+      idD:{
+        title: '客户意愿'
+      },
+      tableDataD:[
+        {desire:'未拔通',num: 21044},
+        {desire:'承诺近期还款',num: 23618},
+        {desire:'未接听',num: 24396},
+        {desire:'延期还款',num: 25903},
+        {desire:'通话中断',num: 26258},
+        {desire:'承诺马上还款',num: 26758},
+      ],
       clientHeight: document.body.clientHeight
     };
   },
   mounted() {
     this.init()
+    this.getTime();
   },
   methods: {
     init() {
     },
+    // 获取右上角当前时间
+    getTime() {
+      if (timeInterval) clearInterval(timeInterval);
+      var timeInterval = setInterval(this.nowTime, 1000);
+    },
+    nowTime() {
+      var myDate = new Date();
+      var y = myDate.getFullYear();
+      var M = myDate.getMonth() + 1; //获取当前月份(0-11,0代表1月)
+      var d = myDate.getDate(); //获取当前日(1-31)
+      var h = myDate.getHours(); //获取当前小时数(0-23)
+      var m = myDate.getMinutes(); //获取当前分钟数(0-59)
+      var s = myDate.getSeconds(); //获取当前秒数(0-59)
+
+      //检查是否小于10
+      M = this.check(M);
+      d = this.check(d);
+      h = this.check(h);
+      m = this.check(m);
+      s = this.check(s);
+      var dateStr = y + "/" + M + "/" + d;
+      var timeStr = h + ":" + m + ":" + s;
+      this.currentTime = Object.assign({}, { date: dateStr, time: timeStr });
+      // 获取当前天气
+      this.getCurrentWeather(myDate.getDate());
+      // document.getElementById("nowtime").innerHTML = "当前时间：" + timestr;
+    },
+    getCurrentWeather(d) {
+      if (this.currentDate === d) {
+        return;
+      }
+      var index = 0;
+      if (d > 6) {
+        this.currentWeather = this.localweather[index];
+        this.currentImg = this.weatherImg[index];
+      } else {
+        this.currentWeather = this.localweather[d];
+        this.currentImg = this.weatherImg[d];
+      }
+    },
+    check(i) {
+      var num = i < 10 ? "0" + i : i;
+      return num;
+    },
+    fromHead( obj ) {
+      console.log(obj)
+      this.headValue = obj.value
+    }
+    
   },
   components: {
     "table-auto": tableAuto,
     "line-right": lineToRight,
-    'big-head': bigHeadVue
+    'big-head': bigHeadVue,
+    'pop-custom': popCustom
   }
 };
 </script>
@@ -330,32 +423,69 @@ export default {
     left: 0;
     bottom: 0;
     color: #fff;
-    background: url('../../assets/images/background.png') 100% 100%;
+    background: url('../../assets/images/p4-background.png') no-repeat 100% 100%;
+    background-size: 100% 100%;
+    .brain-bg {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      color: #fff;
+      background: url('../../assets/images/danao.png') no-repeat center ;
+      background-size: 70%;
+    }
+    .brain-bgg {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      color: #fff;
+      background-size: 70%;
+      }
+    
     .header {
-        height: 8%;
+        height: 7%;
         text-align: center;
-        line-height: 40px;
-        font-size: 18px;
-        font-weight: 600;
-        font-family: sans-serif;
-        letter-spacing: 2px;
-        background: linear-gradient(to right, #fff, #ACC5FD);
-        -webkit-background-clip: text;
-        color: transparent;
+        background: url('../../assets/images/header.png') no-repeat 100% 100%;
+        background-size: 100% 100%;
+        position: relative;
+        .header-bg {
+          display: inline-block;
+          background: url('../../assets/images/header-title.png') no-repeat center;
+          background-size: 40%;
+          width: 30%;
+          height: 100%;
+        }
+        .right-time {
+          width: 33%;
+          text-align: right;
+          position: absolute;
+          top: 10px;
+          right: 0px;
+          font-size: 12px;
+        }
     }
     .main-content {
-      padding: 0 60px;
+      padding: 0 30px;
+      height: 93%;
       div {
         display: inline-block;
+        width: 100%;
       }
       .left {
-        width: 22%;
+        width: 20%;
+        vertical-align: top;
       }
       .center {
-        width: 56%;
+        width: 58%;
+        position: relative;
+        height: 93%;
       }
       .right {
         width: 22%;
+        vertical-align: top;
       }
     }
   }
