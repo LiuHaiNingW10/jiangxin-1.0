be662c6849e6151ecfad48b5c086a748c9e1f31d
liuhaining <2388567009@qq.com>
2020-07-15 19:44:17 +0800

    p4

--- src/components/p4/autoScroll-tableB.vue
+++ src/components/p4/autoScroll-tableB.vue
@@ -60,20 +60,6 @@ export default {
   },
   
   methods: {
-    scroll() {
-      setTimeout(() => {
-        //  这里直接使用了es6的箭头函数，省去了处理this指向偏移问题，代码也比之前简化了很多
-        if (this.activeIndex < this.tableData.length-1) {
-          this.activeIndex += 1;
-        } else {
-          this.activeIndex = 0;
-        }
-        this.needPoint && this.$emit("func", this.tableData[0]);
-        this.tableData.push(this.initData[this.activeIndex]); // 将数组的第一个元素添加到数组的
-        this.tableData.shift(); //删除数组的第一个元素
-        this.animate = false; // margin-top 为0 的时候取消过渡动画，实现无缝滚动
-      }, 500);
-    }
   },
   beforeDestroy() {
     clearInterval(this.timer);
